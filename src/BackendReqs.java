c91d24bca0b419a9c5bdd30977ff410608a37f30
Christopher Lemire <christopher.lemire@gmail.com>
2012-12-01 22:38:24 -0600

    An interface for the Backend

---
+++ src/BackendReqs.java
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: bull
+ * Date: 12/1/12
+ * Time: 10:23 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface BackendReqs {
+    void setAlpha(boolean isAlpha);
+    void setNumeric(boolean isNumeric);
+    void setPunctuation(boolean isPunc);
+    String toString();
+}
