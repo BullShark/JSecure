063c9a9bcc6b4c5bf3d15fedfaf00d02ecdf5aa8
Christopher Lemire <christopher.lemire@gmail.com>
2012-12-01 23:17:56 -0600

    Constructor

--- src/SecurePassword.java
+++ src/SecurePassword.java
@@ -8,6 +8,14 @@
 public class SecurePassword implements BackendReqs {
     private String pass;
     private boolean isAlpha, isNumeric, isPunc;
+
+    public SecurePassword() {
+        pass = "";
+        isAlpha = false;
+        isNumeric = false;
+        isPunc = false;
+    }
+
     @Override
     public void setAlpha(boolean isAlpha) {
         this.isAlpha = isAlpha;
@@ -23,6 +31,7 @@ public class SecurePassword implements BackendReqs {
         this.isPunc = isPunc;
     }
 
+    @Override
     public String toString() {
         return pass;
     }
