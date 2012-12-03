1cfdcbfbc2ca2d884ce6340b544a23a4823de080
Christopher Lemire <christopher.lemire@gmail.com>
2012-12-03 01:32:03 -0600

    Ch01 write some code mofo

--- src/SecurePassword.java
+++ src/SecurePassword.java
@@ -4,6 +4,7 @@
  * Date: 12/1/12
  * Time: 10:58 PM
  * To change this template use File | Settings | File Templates.
+ * todo insert GPLv3 here
  */
 public class SecurePassword implements BackendReqs {
     private String pass;
